d5d0aab89d1b5183034428c43d9766faf9c44bb0
nictrix <nickwillever@gmail.com>
2015-12-23 14:47:27 -0800

    make things similar and optimize the interfaces variable

--- recipes/set_non_nat_vbox_ip.rb
+++ recipes/set_non_nat_vbox_ip.rb
@@ -1,10 +1,11 @@
 # linux
-if node["virtualization"] && node["virtualization"]["system"] == "vbox"
-  interfaces = node["network"]["interfaces"]
-  interface = (interfaces.keys - ["lo", node["network"]["default_interface"]]).first
+interfaces = node["network"]["interfaces"]
 
-  unless interface.empty?
-    interfaces[interface]["addresses"].each do |ip, params|
+if node["virtualization"] && node["virtualization"]["system"] == "vbox"
+  interface_key = (interfaces.keys - ["lo", node["network"]["default_interface"]]).first
+
+  unless interface_key.empty?
+    interfaces[interface_key]["addresses"].each do |ip, params|
       if params["family"] == ("inet")
         node.automatic["ipaddress"] = ip
       end
@@ -12,7 +13,6 @@ if node["virtualization"] && node["virtualization"]["system"] == "vbox"
   end
 # windows
 elsif node["kernel"] && node["kernel"]["cs_info"] && node["kernel"]["cs_info"]["model"] == "VirtualBox"
-  interfaces = node["network"]["interfaces"]
   interface_key = interfaces.keys.last
   node.automatic["ipaddress"] = interfaces[interface_key]["configuration"]["ip_address"][0]
 end
